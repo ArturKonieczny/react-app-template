deb905915c180e493b9c87a304fb7d0304d81cb8
Artur <thetofiq@gmail.com>
2018-01-17 16:33:03 +0100

    App template

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import './style.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>Hello World!</div>
+    );
+  }
+}
+
+ReactDOM.render(<HelloWorld />, document.querySelector('#app'));
